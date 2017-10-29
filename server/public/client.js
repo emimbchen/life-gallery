c7e0e3cc0213a9fa71d2960bb883893bfb803522
Emi Chen <emimbchen@gmail.com>
2017-10-28 21:55:30 -0500

    added get and post requests

--- server/public/client.js
+++ server/public/client.js
@@ -1,9 +1,9 @@
-console.log("js in");
+
 var myApp = angular.module('myApp',[]);
-var pictures = [{ picture: 'pictures/India.jpg', caption: "Blake and I at the Taj Mahal", likes: 0, hidden: true, comments: [], }, { picture: 'pictures/Lucy.jpg', caption: "The lovely Lucy", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Fam.jpg', caption: "My family on vacation in Venice", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Morocco.jpg', caption: "Riding Camels in Morocco", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Shadow.jpg', caption: "Shadow as a goofy kitten", likes: 0, hidden: true, comments: []}, {picture: 'pictures/jellyfish.jpg', caption: "I love the Omaha Zoo", likes: 0, hidden: true, comments: []}]
-myApp.controller('mainController', function(){
+var pictures = "";
+myApp.controller('mainController', function ($http) {
     var controller = this
-    controller.pictures = pictures;
+    refreshPictures();
     controller.showCap = function(picture){
         console.log(picture.hidden);
         picture.hidden = !picture.hidden;
@@ -14,7 +14,7 @@ myApp.controller('mainController', function(){
     }
     controller.addComment = function(picture){
         //picture.comment is what I want to push
-        picture.comments.push(picture.comment)
+        picture.comments.push(picture.comment);
         picture.comment='';
     }
     controller.newPic = function (url, caption) {
@@ -25,8 +25,26 @@ myApp.controller('mainController', function(){
             hidden: true,
             comments: []
         };
-        pictures.push(newPic);
         controller.url = "";
         controller.caption = "";
+        addPicture();
     }
+
+    function refreshPictures() {
+        $http.get('/pictures').then(function (response) {
+            pictures = response.data;
+            console.log(pictures);
+            controller.pictures = pictures;
+        })
+    }
+
+    function addPicture(newPic){
+        $http.post('/pictures', newPic).then(function(response){
+            console.log('Success :)');
+            refreshPictures();
+        }).catch(function(error){
+            console.log('Fail :(');
+        });
+    }
+
 });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
 app.use(express.static('server/public'));
+app.use(bodyParser.json());
+
+var pictures = [{ picture: 'pictures/India.jpg', caption: "Blake and I at the Taj Mahal", likes: 0, hidden: true, comments: [], }, { picture: 'pictures/Lucy.jpg', caption: "The lovely Lucy", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Fam.jpg', caption: "My family on vacation in Venice", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Morocco.jpg', caption: "Riding Camels in Morocco", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/Shadow.jpg', caption: "Shadow as a goofy kitten", likes: 0, hidden: true, comments: [] }, { picture: 'pictures/jellyfish.jpg', caption: "I love the Omaha Zoo", likes: 0, hidden: true, comments: [] }];
+
+//picture post route
+app.post('/pictures', function(req, res){
+    console.log(req.body);
+    var newPicture = req.body;
+    pictures.push(newPicture);
+    console.log(pictures);
+    res.sendStatus(201);
+});
+
+app.get('/pictures', function(req, res){
+    res.send(pictures);
+});
+
 app.listen(port, function(){
     console.log('listening on port: ', port);
 });
